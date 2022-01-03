83986b3feabc4562711e2fd55e5a79423317f9e2
Thiago Mello <thiduzz14@gmail.com>
2022-01-04 00:32:55 +0100

    Scaffold next app and re-arrange eslint

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,11 +1,8 @@
 module.exports = {
-    purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
-    darkMode: false, // or 'media' or 'class'
-    theme: {
-        extend: {},
+    "purge": [ "./src/**/*.{js,jsx,ts,tsx}", "./public/index.html" ],
+    "content": [],
+    "theme": {
+        "extend": {}
     },
-    variants: {
-        extend: {},
-    },
-    plugins: [],
+    "plugins": []
 };
--- truffle-config.js
+++ truffle-config.js
@@ -24,8 +24,8 @@
 // const mnemonic = fs.readFileSync(".secret").toString().trim();
 
 module.exports = {
-  /**
-   * Networks define how you connect to your ethereum client and let you set the
+    /**
+   * Networks define how you connect to your ethereum client-old and let you set the
    * defaults web3 uses to send transactions. If you don't specify one truffle
    * will spin up a development blockchain for you on port 9545 when you
    * run `develop` or `test`. You can ask a truffle command to use a specific
@@ -34,18 +34,18 @@ module.exports = {
    * $ truffle test --network <network-name>
    */
 
-  networks: {
+    "networks": {
     // Useful for testing. The `development` name is special - truffle uses it by default
     // if it's defined here and no other network is specified at the command line.
-    // You should run a client (like ganache-cli, geth or parity) in a separate terminal
+    // You should run a client-old (like ganache-cli, geth or parity) in a separate terminal
     // tab if you use this network and you must also set the `host`, `port` and `network_id`
     // options below to some value.
     //
-    development: {
-      host: "127.0.0.1", // Localhost (default: none)
-      port: 7545, // Standard Ethereum port (default: none)
-      network_id: "*", // Any network (default: none)
-    },
+        "development": {
+            "host": "127.0.0.1", // Localhost (default: none)
+            "port": 7545, // Standard Ethereum port (default: none)
+            "network_id": "*" // Any network (default: none)
+        }
     // Another network with more advanced options...
     // advanced: {
     // port: 8777,             // Custom port
@@ -71,46 +71,46 @@ module.exports = {
     // network_id: 2111,   // This network is yours, in the cloud.
     // production: true    // Treats this network as if it was a public net. (default: false)
     // }
-  },
-
-  // Set default mocha options here, use special reporters etc.
-  mocha: {
-    // timeout: 100000
-  },
-
-  // Configure your compilers
-  compilers: {
-    solc: {
-      version: "0.8.10", // Fetch exact version from solc-bin (default: truffle's version)
-      // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
-      // settings: {          // See the solidity docs for advice about optimization and evmVersion
-      //  optimizer: {
-      //    enabled: false,
-      //    runs: 200
-      //  },
-      //  evmVersion: "byzantium"
-      // }
     },
-  },
 
-  // Truffle DB is currently disabled by default; to enable it, change enabled:
-  // false to enabled: true. The default storage location can also be
-  // overridden by specifying the adapter settings, as shown in the commented code below.
-  //
-  // NOTE: It is not possible to migrate your contracts to truffle DB and you should
-  // make a backup of your artifacts to a safe location before enabling this feature.
-  //
-  // After you backed up your artifacts you can utilize db by running migrate as follows:
-  // $ truffle migrate --reset --compile-all
-  //
-  // db: {
-  // enabled: false,
-  // host: "127.0.0.1",
-  // adapter: {
-  //   name: "sqlite",
-  //   settings: {
-  //     directory: ".db"
-  //   }
-  // }
-  // }
+    // Set default mocha options here, use special reporters etc.
+    "mocha": {
+    // timeout: 100000
+    },
+
+    // Configure your compilers
+    "compilers": {
+        "solc": {
+            "version": "0.8.10" // Fetch exact version from solc-bin (default: truffle's version)
+            // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
+            // settings: {          // See the solidity docs for advice about optimization and evmVersion
+            //  optimizer: {
+            //    enabled: false,
+            //    runs: 200
+            //  },
+            //  evmVersion: "byzantium"
+            // }
+        }
+    }
+
+    // Truffle DB is currently disabled by default; to enable it, change enabled:
+    // false to enabled: true. The default storage location can also be
+    // overridden by specifying the adapter settings, as shown in the commented code below.
+    //
+    // NOTE: It is not possible to migrate your contracts to truffle DB and you should
+    // make a backup of your artifacts to a safe location before enabling this feature.
+    //
+    // After you backed up your artifacts you can utilize db by running migrate as follows:
+    // $ truffle migrate --reset --compile-all
+    //
+    // db: {
+    // enabled: false,
+    // host: "127.0.0.1",
+    // adapter: {
+    //   name: "sqlite",
+    //   settings: {
+    //     directory: ".db"
+    //   }
+    // }
+    // }
 };
